5b3487513678d5928966ef0f91aab85c3706f6a4
Posy94 <posy94500@hotmail.com>
2025-07-19 07:58:01 +0200

    création du menu latéral et du bouton burger

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,60 +1,95 @@
-import React from "react";
+import React, { useState, usestate } from "react";
 import { NavLink } from "react-router";
 
 //CSS
 import style from './Header.module.css'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header>
-            <nav>
-                <ul className="d-flex list-unlisted">
-                    <li className="m-4">
-                        <NavLink to='/'>
-                            Accueil
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/regles'> 
-                            Règles du jeu
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/inscription'> 
-                            Inscription
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/profilJoueur'> 
-                            Profil du joueur
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/connexion'> 
-                            Connexion
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/creationSalon'> 
-                            Création d'un salon
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/cgu'> 
-                            Conditions générales d'utilisation
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/apropos'> 
-                            A Propos
-                        </NavLink>
-                    </li>
-                    <li className="m-4">
-                        <NavLink to='/contact'> 
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
+
+            {/* BOUTON BURGER */}
+
+            <button
+                onClick={toggleMenu}
+                className={style.burgerButton}
+            >
+                <div className={`${style.burgerIcon} ${isMenuOpen ? style.active : ''}`}>  
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>             
+            </button>
+
+            {/* OVERLAY SOMBRE */}
+
+            {isMenuOpen && (
+                <div
+                    className={style.overlay}
+                    onClick={toggleMenu}
+                >                  
+                </div>
+            )}
+
+            {/* MENU COULISSANT*/}
+
+            <nav className={`${style.slideMenu} ${isMenuOpen ? style.open : ''}`}>
+
+                <ul className={style.menuList}>
+
+                        <li>
+                            <NavLink to='/' onClick={toggleMenu}>
+                                Accueil
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/regles' onClick={toggleMenu}> 
+                                Règles du jeu
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/inscription' onClick={toggleMenu}> 
+                                Inscription
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/profilJoueur' onClick={toggleMenu}> 
+                                Profil du joueur
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/connexion' onClick={toggleMenu}> 
+                                Connexion
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/creationSalon' onClick={toggleMenu}> 
+                                Création d'un salon
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/cgu' onClick={toggleMenu}> 
+                                Conditions générales d'utilisation
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/apropos' onClick={toggleMenu}> 
+                                A Propos
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contact' onClick={toggleMenu}> 
+                                Contact
+                            </NavLink>
+                        </li>
+
+                    </ul>
             </nav>
         </header>
     )
